20b9a88deb8c3ceed12060ff17ba870a01fa1307
«Sviatoslav» <ksvyat21@gmail.com>
2024-05-26 12:37:41 +0200

    feat: Configure GoogleStrategy
    This commit configures the Google OAuth 2.0 strategy for user authentication

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,6 +1,8 @@
 const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+require("dotenv").config()
 
 module.exports = function(passport) {
     passport.use(
@@ -22,6 +24,34 @@ module.exports = function(passport) {
             }
         })
     );
+
+    // Configure GoogleStrategy
+    passport.use(new GoogleStrategy({
+            // provide GOOGLE_CLIENT_ID
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            // provide GOOGLE_CLIENT_SECRET
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            // specify callback
+            callbackURL: "http://localhost:5001/auth/google/callback"
+        },
+        async (token, tokenSecret, profile, done) => {
+            console.log("Google profile:", profile);
+            try {
+                let user = await User.findOne({ googleId: profile.id });
+                if (!user) {
+                    user = new User({
+                        googleId: profile.id,
+                        email: profile.emails[0].value,
+                    });
+
+                    await user.save();
+                }
+                return done(null, user);
+            } catch (err) {
+                return done(err, false);
+            }
+        }
+    ));
 
     passport.serializeUser((user, done) => {
         done(null, user.id);
