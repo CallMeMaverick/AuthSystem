ec73abdbe4976169fdee48aba09009d724bf0af9
«Sviatoslav» <ksvyat21@gmail.com>
2024-05-26 14:05:37 +0200

    config: Set up port

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res) {
   res.render('signup', { title: 'Error', errorMessage: err.message });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
