33a473c9a960f376e4bc00c21a3b37ffd8ebe745
«Sviatoslav» <ksvyat21@gmail.com>
2024-05-26 12:38:40 +0200

    feat: Add Google OAuth routes
    Introduced routes for handling Google OAuth authentication in the application.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require("passport")
 const router = express.Router();
 const userController = require("../controllers/userController");
 const { ensureAuthenticated } = require("../middleware/auth");
@@ -6,9 +7,26 @@ const { ensureAuthenticated } = require("../middleware/auth");
 router.post('/signup', userController.signUp);
 router.post("/signin", userController.signIn);
 
+// Google OAuth routes
+router.get('/auth/google', (req, res, next) => {
+    next();
+}, passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/callback',
+    (req, res, next) => {
+        next();
+    },
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        res.redirect('/protected'); // Redirect to protected route after successful authentication
+    }
+);
+
+
 // Protected route
 router.get('/protected', ensureAuthenticated, userController.protected);
 
+// Logout route
 router.get('/logout', userController.logOut);
 
 module.exports = router;
